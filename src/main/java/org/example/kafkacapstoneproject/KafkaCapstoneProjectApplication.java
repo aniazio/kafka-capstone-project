e9c287e273889ea3350a062f628844bb8d69ba4c
aziolkowska <aziolkowska@griddynamics.com>
2026-01-26 10:03:42 +0100

    initial commit

---
+++ src/main/java/org/example/kafkacapstoneproject/KafkaCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.kafkacapstoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCapstoneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCapstoneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kafkacapstoneproject/KafkaCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kafkacapstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
