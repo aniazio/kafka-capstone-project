da3f9fea003712c7c45e89afb2f50903f96f597d
aziolkowska <aziolkowska@griddynamics.com>
2026-02-10 19:54:41 +0100

    merge metrics into one topic

--- src/main/java/org/example/kafkacapstoneproject/listener/BasicListener.java
+++ src/main/java/org/example/kafkacapstoneproject/listener/BasicListener.java
@@ -1,7 +1,7 @@
 package org.example.kafkacapstoneproject.listener;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.kafkacapstoneproject.metrics.MetricsProcessingStream;
+import org.example.kafkacapstoneproject.config.AppConfig;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class BasicListener {
 
-    @KafkaListener(id = "basic-listener", topics = {MetricsProcessingStream.TOTAL_COMMITS, MetricsProcessingStream.TOTAL_COMMITERS, MetricsProcessingStream.COMMITS_PER_LANGUAGE, MetricsProcessingStream.TOP_FIVE_CONTRIBUTORS_BY_COMMITS, MetricsProcessingStream.NUMBER_OF_LINES_EDITED, MetricsProcessingStream.INCREMENT_OF_LINES, MetricsProcessingStream.TOP_FIVE_CONTRIBUTORS_BY_LINES_EDITED, MetricsProcessingStream.PERCENT_OF_COMMITS_WITH_THE_SAME_AUTHOR_AND_COMMITER})
+    @KafkaListener(id = "basic-listener", topics = AppConfig.GITHUB_METRICS_TOPIC)
     public void listen(String message) {
         log.info("Received message: {}", message);
     }
