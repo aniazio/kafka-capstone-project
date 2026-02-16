4bf7fd37ea9dcc3d496bbe09a6e4af35549f1e04
aziolkowska <aziolkowska@griddynamics.com>
2026-02-16 11:07:45 +0100

    tries to optimize

--- src/main/java/org/example/kafkacapstoneproject/github/GithubApiAdapter.java
+++ src/main/java/org/example/kafkacapstoneproject/github/GithubApiAdapter.java
@@ -7,6 +7,8 @@ import org.example.kafkacapstoneproject.model.GithubCommitMessage;
 import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHCommitSearchBuilder;
 import org.kohsuke.github.GHDirection;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHRepositorySearchBuilder;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.PagedIterator;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -36,10 +40,24 @@ public class GithubApiAdapter {
 
 
         try {
-            PagedIterator<GHCommit> it = search.list().iterator();
-            while (it.hasNext()) {
-                GHCommit commit = it.next();
-                results.add(new GithubCommitMessage(commit, commit.getOwner().getLanguage()));
+            List<GHRepository> repos = new ArrayList<>();
+            PagedIterator<GHCommit> itCommits = search.list().iterator();
+            log.info("List received");
+            while (itCommits.hasNext()) {
+                GHCommit commit = itCommits.next();
+                repos.add(commit.getOwner());
+            }
+            log.info("Repos collected");
+
+            final GHRepositorySearchBuilder searchRepo = github.searchRepositories();
+            repos.forEach(repository -> searchRepo.repo(repository.getName()));
+
+            Map<String, String> repoToLanguage = searchRepo.list().toSet().stream().collect(Collectors.toMap(GHRepository::getName, GHRepository::getLanguage));
+            log.info("Map of repos constructed");
+            itCommits = search.list().iterator();
+            while (itCommits.hasNext()) {
+                GHCommit commit = itCommits.next();
+                results.add(new GithubCommitMessage(commit, repoToLanguage.get(commit.getOwner().getName())));
             }
         } catch (IOException e) {
             log.error("Error during search", e);
--- src/main/java/org/example/kafkacapstoneproject/github/GithubCommitsMockProducer.java
+++ src/main/java/org/example/kafkacapstoneproject/github/GithubCommitsMockProducer.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.kafkacapstoneproject.config.AppConfig;
 import org.example.kafkacapstoneproject.model.GithubCommitMessage;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +14,7 @@ public class GithubCommitsMockProducer {
 
     private final KafkaTemplate<String, GithubCommitMessage> kafkaTemplate;
 
-    @Scheduled(fixedRate = 1000)
+    //@Scheduled(fixedRate = 1000)
     public void send() {
         log.info("Sending commit message");
         kafkaTemplate.send(AppConfig.GITHUB_COMMITS_TOPIC, GithubCommitMessage.random());
